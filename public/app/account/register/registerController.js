e95624929194f335a5aae78762e60ab79317428f
Kent Baker <kenneth.ray.baker@gmail.com>
2016-02-03 22:13:37 -0600

    removed first Name and Last name from Registration and replaced it with Username and added Regex pattern to the Zip Code

---
+++ public/app/account/register/registerController.js
@@ -0,0 +1,18 @@
+(function () {
+    'use strict';
+
+    angular
+            .module('register.RegisterCtrl', [])
+            .controller('RegisterCtrl', RegisterCtrl);
+
+    function RegisterCtrl() {
+       var vm = this;
+        
+        vm.registerSubmit = function(){
+            
+        };
+    }
+
+
+})();
+
--- public/app/account/register/registerModule.js
+++ public/app/account/register/registerModule.js
@@ -1,13 +1,15 @@
 (function () {
     'use strict';
-    
-    angular.module('account.register', ['ngRoute'])
-            .config(['$routeProvider',function ($routeProvider) {
+
+    angular.module('account.register', ['ngRoute', 'register.RegisterCtrl'])
+            .config(['$routeProvider', function ($routeProvider) {
                     $routeProvider
-                    .when('/register', {
-                        templateUrl: 'app/account/register/register.html',
-                        resolve:[]
-                    });
+                            .when('/register', {
+                                templateUrl: 'app/account/register/register.html',
+                                controller: 'RegisterCtrl',
+                                controllerAs: 'register',
+                                resolve: []
+                            });
                 }]);
 })();
 
