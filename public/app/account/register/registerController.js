b3f30af8f4394b9d0b133fe3e8feb5b30eb06cb5
Kent Baker <kenneth.ray.baker@gmail.com>
2016-02-05 23:20:46 -0600

    updated the index and registerController with angular-storage

--- public/app/account/register/registerController.js
+++ public/app/account/register/registerController.js
@@ -2,11 +2,15 @@
     'use strict';
 
     angular
-            .module('register.RegisterCtrl', [])
+            .module('register.RegisterCtrl', ['ngRoute', 'angular-storage'])
             .controller('RegisterCtrl', RegisterCtrl);
 
     function RegisterCtrl() {
        var vm = this;
+       
+       var user = {};
+       
+       
         
         vm.registerSubmit = function(){
             
