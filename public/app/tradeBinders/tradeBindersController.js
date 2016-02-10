ed8147c258c18f5442da8042e5226bfd5aff8da4
Kent Baker <kenneth.ray.baker@gmail.com>
2016-02-09 23:05:26 -0600

    Mass card entry

---
+++ public/app/tradeBinders/tradeBindersController.js
@@ -0,0 +1,34 @@
+(function () {
+    'use strict';
+
+    angular.module('TradeBinders.TradeBindersController', ['TradeBinder.TradeBindersFactory'])
+            .controller('TradeBindersController', TradeBindersController);
+
+    function TradeBindersController(TradeBindersFactory) {
+        var vm = this;
+
+        vm.cards = TradeBindersFactory.getCards();
+
+        vm.addCards = function (cards) {
+            if (cards.length > 0) {
+                TradeBindersFactory.addCards(cards);
+                vm.cards = TradeBindersFactory.getCards();
+                vm.addList = [];
+            }
+        };
+
+        vm.removeCard = function (card) {
+            TradeBindersFactory.removeCard(card);
+        };
+
+        vm.upQuantity = function (card) {
+            TradeBindersFactory.upCardQuantity(card);
+        };
+
+        vm.lowerQuantity = function (card) {
+            TradeBindersFactory.lowerCardQuantity(card);
+        };
+
+
+    }
+})();
