fbf09341e36b7141c4ed7f9437730d22f52050f1
Dustin Halbrooks <halbrooksdg@lamchanneld.org>
2016-02-13 07:37:09 -0600

    Removed duplicate path.

--- karma.conf.js
+++ karma.conf.js
@@ -18,10 +18,8 @@ module.exports = function(config) {
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
 
-      //Serve up the actual files to be tested
-      'public/app/**/*.js',
-      //Serve up the test files
-      'public/**/spec/*.js'
+      //Serve up the actual files and test files to be tested
+      'public/app/**/*.js'
     ],
 
 
