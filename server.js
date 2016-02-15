8e88509551f960199b9959a729f9918422c66ddc
Dustin Halbrooks <halbrooksdg@lamchanneld.org>
2016-02-15 07:14:29 -0600

    Added templateCache to cache all of our html on startup for node.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'html');
 
 //Should be very simple to make some service calls this way
