81709465929c61819ae6312af83453adda227d8d
th3ALPHAnerd <p1ck37t@gmail.com>
2016-02-25 23:53:43 -0600

    Fixed CORS issue on search page.

--- public/app/search/searchModule.js
+++ public/app/search/searchModule.js
@@ -1,18 +1,16 @@
 (function () {
-    'use strict';
+  'use strict';
 
-    angular.module('search', ['ui.router', 'search.SearchController'])
-            .config(['$stateProvider', '$httpProvider', function ($stateProvider, $httpProvider) {
-                    $stateProvider
-                            .state('search', {
-                                url: '/tradeBinder/search',
-                                templateUrl: 'app/search/search.html',
-                                controller: 'SearchController',
-                                controllerAs: 'search',
-                                resolve: []
-                            });
-                            
-                            delete $httpProvider.defaults.headers.common['X-Requested-With'];
-                }]);
+  angular.module('search', ['ui.router', 'search.SearchController'])
+  .config(['$stateProvider', '$httpProvider', function ($stateProvider, $httpProvider) {
+    $stateProvider
+    .state('search', {
+      url: '/tradeBinder/search',
+      templateUrl: 'app/search/search.html',
+      controller: 'SearchController',
+      controllerAs: 'search',
+      resolve: []
+    });
+  }]);
 })();
 
