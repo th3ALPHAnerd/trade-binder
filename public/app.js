1b9c272250a479313e8b25b703a76f9b963a135f
Kent Baker <kenneth.ray.baker@gmail.com>
2016-02-26 01:05:13 -0600

    added logout feature. - logout link in navbar - removes jwt from local storage - redirects to home page after logout

--- public/app.js
+++ public/app.js
@@ -1,51 +1,61 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  angular.module('app', [
-    'ui.router',
-    'angular-jwt',
-    'angular-storage',
-    'home',
-    'tradeBinders',
-    'account',
-    'cardShops',
-    'search'
-  ])
-  .config(['jwtInterceptorProvider', '$httpProvider', '$locationProvider',
-          function myConfig(jwtInterceptorProvider, $httpProvider, $locationProvider) {
-            jwtInterceptorProvider.tokenGetter = function (store) {
-              return store.get('jwt');
-            };
-            $httpProvider.interceptors.push('jwtInterceptor');
+    angular.module('app', [
+        'ui.router',
+        'angular-jwt',
+        'angular-storage',
+        'home',
+        'tradeBinders',
+        'account',
+        'cardShops',
+        'search'
+    ])
+            .controller('appController', ['UserService', '$state', appController])
 
-            // $locationProvider.html5Mode({
-            //   enabled: true,
-            //   requireBase: false
-            // });
-          }])
-          .run(function ($rootScope, $state, $http, $templateCache, store, jwtHelper) {
-            $rootScope.$on('$stateChangeStart', function (e, to) {
-              if (to.data && to.data.requiresLogin) {
-                if (!store.get('jwt') || jwtHelper.isTokenExpired(store.get('jwt'))) {
-                  e.preventDefault();
-                  $state.go('login');
-                }
-              }
-              $http.get('app/account/register/register.html', {
-                cache: $templateCache
-              });
-              $http.get('app/account/login/login.html', {
-                cache: $templateCache
-              });
-              $http.get('app/tradeBinders/tradeBinders.html', {
-                cache: $templateCache
-              });
-              $http.get('app/search/search.html', {
-                cache: $templateCache
-              });
-              $http.get('app/cardShops/cardShops.html', {
-                cache: $templateCache
-              });
+            .config(['jwtInterceptorProvider', '$httpProvider', '$locationProvider', '$stateProvider',
+                function myConfig(jwtInterceptorProvider, $httpProvider, $locationProvider, $stateProvider) {
+                    jwtInterceptorProvider.tokenGetter = function (store) {
+                        return store.get('jwt');
+                    };
+                    $httpProvider.interceptors.push('jwtInterceptor');
+
+                    $stateProvider
+                            .state('logout', {
+                                controller: 'appController',
+                            });
+
+                }])
+            .run(function ($rootScope, $state, $http, $templateCache, store, jwtHelper) {
+                $rootScope.$on('$stateChangeStart', function (e, to) {
+                    if (to.data && to.data.requiresLogin) {
+                        if (!store.get('jwt') || jwtHelper.isTokenExpired(store.get('jwt'))) {
+                            e.preventDefault();
+                            $state.go('login');
+                        }
+                    }
+                    $http.get('app/account/register/register.html', {
+                        cache: $templateCache
+                    });
+                    $http.get('app/account/login/login.html', {
+                        cache: $templateCache
+                    });
+                    $http.get('app/tradeBinders/tradeBinders.html', {
+                        cache: $templateCache
+                    });
+                    $http.get('app/search/search.html', {
+                        cache: $templateCache
+                    });
+                    $http.get('app/cardShops/cardShops.html', {
+                        cache: $templateCache
+                    });
+                });
             });
-          });
+
+    function appController(UserService, $state) {
+        UserService.logout();
+        $state.go('home');
+    };
+
+
 })();
