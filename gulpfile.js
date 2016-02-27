3a96a645f3074d20f795cef82ad2cbbc1fc2c556
Kent Baker <kenneth.ray.baker@gmail.com>
2016-02-27 12:45:42 -0600

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var Server = require('karma').Server;
+var del = require('del');
+var sourcemaps = require('gulp-sourcemaps');
+var bower = require('gulp-bower');
+//var unique = require('uniq')
+
+
+gulp.task('bower', function(){
+  return bower();
+});
+
+//Clean js in dist folder
+gulp.task('cleanJs', function (cb) {
+    return del(['public/dist/**/*.js', '!public/dist'], cb);
+});
+
+//Clean dist folder
+gulp.task('clean:dist', function (cb) {
+    return del(['public/dist/**', '!public/dist'], cb);
+});
+
+/**
+ * Run test once and exit
+ */
+gulp.task('test', function (cb) {
+    new Server({
+        configFile: __dirname + '/karma.conf.js',
+        singleRun: true
+    }, cb).start();
+});
+
+// Lint Task
+gulp.task('lint', function () {
+    return gulp.src('public/src/**/*.js')
+            .pipe(jshint())
+            .pipe(jshint.reporter('default'));
+});
+
+
+
+// Concatenate & Minify JS
+gulp.task('scripts', ['cleanJs'], function () {
+    return gulp.src(
+            [
+                'public/**/*.js',
+                'public/**/*.*.js',
+		'!public/assets/**',
+		'!public/dist',
+                '!public/**/*.spec.js'
+            ])
+            .pipe(sourcemaps.init())
+            .pipe(concat('all.js'))
+            .pipe(rename('app.min.js'))
+            .pipe(uglify())
+            .pipe(sourcemaps.write('/'))
+            .pipe(gulp.dest('public/dist'));
+});
+
+//Default Task
+gulp.task('default', ['lint', 'scripts', 'test']);
--- public/app.js
+++ public/app.js
@@ -8,6 +8,7 @@
         'home',
         'tradeBinders',
         'account',
+        'account.UserService',
         'cardShops',
         'search'
     ])
