630777bb7872e50d5efac945c55e7c146a9cc06f
Kent Baker <kenneth.ray.baker@gmail.com>
2016-03-04 20:12:10 -0600

    now once you click on the you are logged in and sent to the home page.

--- public/app/account/verifyEmail/verifyEmailController.js
+++ public/app/account/verifyEmail/verifyEmailController.js
@@ -1,22 +1,37 @@
 (function () {
     'use strict';
 
-    angular.module('account.verifyEmail.verifyEmailController', ['account.verifyEmail.verifyEmailService'])
-            .controller('verifyEmailController', ['$state', 'verifyEmailService', '$stateParams', verifyEmailController]);
+    angular.module('account.verifyEmail.verifyEmailController', ['account.verifyEmail.verifyEmailService', 'angular-jwt', 'account.UserService'])
+            .controller('verifyEmailController', ['$state', 'verifyEmailService', '$stateParams', 'jwtHelper', 'UserService', verifyEmailController]);
 
-    function verifyEmailController($state, verifyEmailService, $stateParams) {
+    function verifyEmailController($state, verifyEmailService, $stateParams, jwtHelper, UserService) {
         var vm = this;
 
         vm.token = $stateParams.token;
 
+        vm.loggedIn = UserService.isLoggedIn();
+
         function verifyEmail() {
             verifyEmailService.pushToken(vm.token);
-            
-            
 
-            $state.go('home');
+            var user = jwtHelper.decodeToken(vm.token);
+
+            vm.dataLoading = true;
+            var data = {
+                username: user.username,
+                password: user.password
+            };
+
+            UserService.login(data).then(function (response) {
+                if (response.success) {
+                    $state.go('home');
+                } else {
+                    alert(response.message);
+                    vm.dataLoading = false;
+                }
+            });
+
         }
-
 
         vm.verifyEmailSubmit = verifyEmail;
     }
